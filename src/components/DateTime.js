19f0c913fa8ac47565881d5b4a89d3b2558ae3a1
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-03-14 20:35:19 +0300

    use setInterval for date time at footer

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
 const DateTime = () => {
+    setInterval(updateTime, 1000);
 
-    const date = new Date()
+    const [currentTime, setTime] = useState(new Date().toLocaleTimeString());
+    const [currentDate, setDate] = useState(new Date().toLocaleDateString());
+
+
+    function updateTime() {
+        setTime(new Date().toLocaleTimeString());
+        setDate(new Date().toLocaleDateString());
+
+    }
+
     return (
-        <div className="date-time">
-            {`${date.toLocaleDateString()} ${date.toLocaleTimeString()}`}
+
+        <div>
+            <p>{currentDate}</p>
+            <p>{currentTime}</p>
         </div>
-    )
+
+    );
 }
 
 export default DateTime;
